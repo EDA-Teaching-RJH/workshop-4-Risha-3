c2ea72b77230407d87b5a146c20fef21d4b4f445
Risha-3 <rs989@kent.ac.uk>
2024-10-18 11:47:08 +0000

    Completed Task 1

--- camel.py
+++ camel.py
@@ -1,1 +1,14 @@
+def camel_to_snake(camel_case):
+    snake_case = ''
+    for character in camel_case:
+        if character.isupper():
+           snake_case += '_' + character.lower()
+        else:
+           snake_case += character
+    return snake_case
+    
+
+camel_input = input("Please enter name of a variable in camel case: ") # preferredFirstName
+snake_output = camel_to_snake(camel_input)
+print(snake_output)
 
